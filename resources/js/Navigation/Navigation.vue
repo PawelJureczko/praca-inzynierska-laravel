69d0e9f5a2fb8426c5597f5758214ae399e000cd
PawelJureczko <jureczko.pawel@gmail.com>
2024-03-03 23:41:00 +0100

    added some changes for login and logout for the target structure

---
+++ resources/js/Navigation/Navigation.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav class="flex justify-between items-center">
+        <LogoIcon />
+
+        <ul class="flex items-center gap-12">
+            <li>
+                <span>Mój terminarz</span>
+            </li>
+            <li>
+                <span>Moi uczniowie</span>
+            </li>
+            <li>
+                <span>Wiadomości</span>
+            </li>
+            <li>
+                <span>Raporty i statystyki</span>
+            </li>
+            <li>
+                <span>Mój profil</span>
+            </li>
+        </ul>
+        <Link :href="route('logout')" method="post">Wyloguj</Link>
+
+    </nav>
+</template>
+
+<script setup>
+import LogoIcon from "@/Components/Icons/LogoIcon.vue";
+import {Link} from "@inertiajs/vue3";
+
+</script>
+
+<style scoped>
+
+</style>
