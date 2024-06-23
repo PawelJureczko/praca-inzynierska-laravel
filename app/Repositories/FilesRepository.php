86eecb39261876a291f7cd3fd86d3473e4d20077
PawelJureczko <jureczko.pawel@gmail.com>
2024-06-23 11:23:47 +0200

    moved logic from controler to repository

---
+++ app/Repositories/FilesRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Support\Facades\DB;
+
+class FilesRepository
+{
+    public function uploadFile($request) {
+        if ($request->hasFile('files')) {
+            $files = $request->file('files');
+            foreach ($files as $file) {
+                // Zapisz plik
+                $hashedName = $file->hashName();
+                $path = $file->storeAs('public/uploads', $hashedName);
+
+                // Zapisz informacje o pliku w bazie danych
+                DB::table('files')->insert([
+                    'filename' => $file->getClientOriginalName(),
+                    'hashed_filename' => $hashedName,
+                    'path' => $path,
+                    'uploaded_by' => auth()->id(), // Jeśli masz autoryzację
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+            return response()->json([
+                'status' => 'ok',
+                'message' => 'Pliki zostały pomyślnie przesłane.']);
+        }
+        return response()->json(['message' => 'Wystąpił niespodziewany błąd.'], 500);
+    }
+}
